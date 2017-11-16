72d77e14d82d38218db279ffe01b77b14a9ba39d
Daniel Setton <dsetton@users.noreply.github.com>
2017-11-16 10:39:07 -0200

    Update Task.kt

--- lesson1/task4/src/Task.kt
+++ lesson1/task4/src/Task.kt
@@ -1,9 +1,11 @@
-fun foo(name: String, number: Int, toUpperCase: Boolean) =
+// o objetivo desse exercício era trocar a declaração da função "foo" para usar o argumento "default" da propriedade "number"
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean) =
         (if (toUpperCase) name.toUpperCase() else name) + number
 
 fun useFoo() = listOf(
-        foo("a", 42, false),
+        foo("a", false),
         foo("b", 1, false),
-        foo("c", number = 42, toUpperCase = true),
+        foo("c", toUpperCase = true),
         foo(name = "d", number = 2, toUpperCase = true)
 )
