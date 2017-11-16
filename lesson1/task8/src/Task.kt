e6ac097bdee6664389b6214799531ecc383455e1
Daniel Setton <dsetton@users.noreply.github.com>
2017-11-16 10:44:54 -0200

    Update Task.kt

--- lesson1/task8/src/Task.kt
+++ lesson1/task8/src/Task.kt
@@ -1,13 +1,22 @@
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ){
-    if (client == null || message == null) return
+        // não precisa fazer a checagem se o client é "null". Basta usar o "?"
+        
+    //if (client == null || message == null) return
 
-    val personalInfo = client.personalInfo?: return
+    //val personalInfo = client.personalInfo?: return
 
-    val email = personalInfo?.email?: return
+    //val email = personalInfo?.email?: return
 
-    mailer.sendMessage(email, message)
+    //mailer.sendMessage(email, message)
+        
+        val email = client?.personlInfo?.email          //veja como o código é menos verborrágico que a versão Java
+        if(email != null && message != null){
+                mailer.sendMessage(email, message)
+        }
+        
+        
     /*
     copy+paste+conversion+renomear("getPersonalInfo" -> "personalInfo")
     * if (client == null || message == null) return
